c30f952832177604feefa997415b7425a03a3ece
Zetix <zetixr@mail.ru>
2026-07-10 17:47:14 +0500

    NavBar made

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,7 +4,15 @@ import "../assets/navBar.css"
 const navBar = () => {
   return (
     <div className='navbar'>
-        <img src={logo} alt="" className='logo'/>
+      <img src={logo} alt="" className='logo' />
+      <div className="nav-text">
+        <a href="">О КОМПАНИИ</a>
+        <a href="">АССОРТИМЕНТ</a>
+        <a href="">КАК ИСПОЛЬЗОВАТЬ</a>
+        <a href="">КОНТАКТЫ</a>
+        <a href="">ГДЕ КУПИТЬ</a>
+      </div>
+      <a href="" className='tel-number'>+7(952)282-28-23</a>
     </div>
   )
 }
